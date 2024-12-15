1f1072af30117dbbade78e1951cc215f25875d73
Yash Kale <67534207+inferno080@users.noreply.github.com>
2024-12-15 17:49:12 -0500

    Create MinimumSwaps2.js

---
+++ 2024/December/MinimumSwaps2.js
@@ -0,0 +1,13 @@
+function minimumSwaps(arr) {
+    let swaps = 0;
+    for(let i = 0; i < arr.length; ++i) {
+        while(arr[i] != i+1) {
+            // can't swap with [[],[]] = [[],[]]. Messes up refs
+            let valAtSwapIndex = arr[arr[i]-1];
+            arr[arr[i]-1] = arr[i];
+            arr[i] = valAtSwapIndex;
+            swaps+=1;
+        }
+    }
+    return swaps;
+}
