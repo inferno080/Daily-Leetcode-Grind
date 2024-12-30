566760b48b79e1cca95914684e2577e8741f0d35
Yash Kale <67534207+inferno080@users.noreply.github.com>
2024-12-30 14:05:25 +0530

    Create CounterII.ts

---
+++ 2024/Javascript/CounterII.ts
@@ -0,0 +1,25 @@
+// https://www.youtube.com/watch?v=UXNXKGFZD08&ab_channel=NeetCodeIO
+type Counter = {
+    increment: () => number,
+    decrement: () => number,
+    reset: () => number,
+}
+
+function createCounter(init: number): Counter {
+    let count = init;
+    return {
+        increment: () => ++count,
+        decrement: () => --count,
+        reset: () => {
+            count = init;
+            return count;
+        }
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
