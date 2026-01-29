63109159c7cbb07e78c3b2dfc2293c395372fa7a
Yash Kale <67534207+inferno080@users.noreply.github.com>
2026-01-29 10:37:27 -0500

    Add setCancellableInterval function

---
+++ 2026/January/GFE/CanellableInterval.ts
@@ -0,0 +1,10 @@
+export default function setCancellableInterval(
+  callback: Function,
+  delay?: number,
+  ...args: Array<any>
+): () => void {
+  const timerId = setInterval(callback, delay, ...args);
+  return ()=>{
+    clearInterval(timerId)
+  }
+}
